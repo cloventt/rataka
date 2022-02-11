dd3b82e85134aa709105adbc07ef413a4c2172f5
David Palmer <david.palmer@adscale.co.nz>
2022-02-11 19:58:21 +1300

    barebones app skellington in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogListComponent } from './log-list/log-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogListComponent,
   ],
   imports: [
     BrowserModule
