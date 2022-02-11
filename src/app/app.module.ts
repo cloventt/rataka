3c5bec3f18a5ffca51186196ea0d240f488fe261
David Palmer <david.palmer@adscale.co.nz>
2022-02-12 10:50:00 +1300

    add ui-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -7,6 +8,12 @@ import { LogListComponent } from './log-list/log-list.component';
 import { DataService } from './data.service';
 import { LogListRowComponent } from './log-list/log-list-row/log-list-row.component';
 import { FormModalComponent } from './new/form-modal/form-modal.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -15,9 +22,11 @@ import { FormModalComponent } from './new/form-modal/form-modal.component';
     LogListComponent,
     LogListRowComponent,
     FormModalComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
