f5605f2a392e4c90a489bcfad2e9006f6e44d8be
David Palmer <david.palmer@adscale.co.nz>
2022-02-13 10:36:32 +1300

    auto-dismiss the add new contact modal on save

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { ContactLogEntry } from '../db';
 
@@ -10,13 +11,12 @@ import { ContactLogEntry } from '../db';
 })
 export class AddComponent implements OnInit {
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(data: any): void {
-    console.debug(data);
+  onSubmit(data: NgForm): void {
     const newRecord: ContactLogEntry = {
       timestamp: new Date().toISOString(),
       callsign: data.value.callsign,
@@ -27,8 +27,16 @@ export class AddComponent implements OnInit {
       rstSent: data.value.rxr + data.value.rxs,
       notes: data.value.notes,
     }
-    console.debug(newRecord);
-    this.dataService.addEntry(newRecord);
+    this.dataService
+      .addEntry(newRecord)
+      .then(
+        () => {  // onfullfilled
+          data.resetForm();
+          this.router.navigate(['/']);
+        },
+        () => { // onrejected
+          alert("Failed to store new contact");
+        });
   }
 
 }
