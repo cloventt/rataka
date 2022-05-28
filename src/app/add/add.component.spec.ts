0937b2342b6d723aae2ee24d37f81c06b87ba91d
David Palmer <david.palmer@adscale.co.nz>
2022-05-29 11:07:00 +1200

    fix failing tests due to import issues

--- src/app/add/add.component.spec.ts
+++ src/app/add/add.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AddComponent } from './add.component';
 
@@ -8,7 +10,8 @@ describe('AddComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddComponent ]
+      declarations: [ AddComponent ],
+      imports: [ RouterTestingModule, FormsModule ]
     })
     .compileComponents();
   });
--- src/app/github.service.spec.ts
+++ src/app/github.service.spec.ts
@@ -1,12 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GithubService } from './github.service';
 
-describe('AuthService', () => {
+describe('GithubService', () => {
   let service: GithubService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, HttpClientTestingModule ]
+    });
     service = TestBed.inject(GithubService);
   });
 
