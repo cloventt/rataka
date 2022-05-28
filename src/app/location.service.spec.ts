f6a78404e676fae51c46974cd9b32ddc4e866dcb
David Palmer <david.palmer@adscale.co.nz>
2022-05-29 11:07:24 +1200

    add some unit tests for location service conversions

--- src/app/location.service.spec.ts
+++ src/app/location.service.spec.ts
@@ -13,4 +13,17 @@ describe('LocationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should correctly convert position to maidenhead', () => {
+    expect(LocationService.toMaidenhead(172, -47)).toEqual('RE66');
+    expect(LocationService.toMaidenhead(46.951, 7.439)).toEqual('JN36rw');
+  })
+
+  it('should correctly convert decimal to degrees', () => {
+    expect(LocationService.toDegrees(172)).toEqual([172, 0, 0]);
+    expect(LocationService.toDegrees(46.951)).toEqual([46, 57, 4]);
+    expect(LocationService.toDegrees(-7.439)).toEqual([-7, 26, 19]);
+    expect(LocationService.toDegrees(-14.265)).toEqual([-14, 15, 55]);
+    expect(LocationService.toDegrees(-32.251)).toEqual([-32, 15, 5]);
+  })
 });
